9d129b7c569ff1b44223be443a2a6e0fab98f3f2
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2025-05-26 19:31:10 +0300

    Add a dummy model for testing purposes

---
+++ dummy_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+class DModel:
+    "A very simple binary classifier model emulator. Predictions are generated according to a predefined sequence"
+
+    def __init__(self, probability_seq=(), name="DModel") -> None:
+        "The probability sequence is that of class 1"
+        self.index = 0
+        self.name = name
+        self.set_sequence(probability_seq)
+
+    def set_sequence(self, probability_seq: list | tuple):
+        "The probability sequence is that of class 1"
+        self.probability_seq = tuple(probability_seq)
+        self.max_index = len(probability_seq)
+
+        for prob in probability_seq:
+            if prob < 0 or prob > 1:
+                raise ValueError("All probabilitites should be in range [0;1]")
+
+    def predict(self, x):
+        # x is ignored intentionally
+        return round(self.probability_seq[self.index])
+
+    def predict_proba(self, x):
+        # Provide the raw probability of both classes, intended to imitate sklearn
+        return (1-self.probability_seq[self.index], self.probability_seq[self.index])
+
+    def reset(self):
+        self.index = 0
+
+    def __str__(self) -> str:
+        return "Model:%s, values: %s" % (self.name, self.probability_seq)
