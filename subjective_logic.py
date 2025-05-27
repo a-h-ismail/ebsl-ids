de5c67fd230f56b1a5ee5d6f86c30745a3220a78
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2025-05-27 09:15:01 +0300

    Initial implementation of ensemble learning with subjective logic.

--- subjective_logic.py
+++ subjective_logic.py
@@ -7,13 +7,16 @@ class Opinion:
     def __init__(self, belief: float = 0, disbelief: float = 0, uncertainty: float = 1, base_rate: float = 0) -> None:
         self.set_parameters(belief, disbelief, uncertainty, base_rate)
 
-    def set_parameters(self, belief: float, disbelief: float, uncertainty: float, base_rate: float):
+    def set_parameters(self, belief: float, disbelief: float, uncertainty: float, base_rate: float = -1):
         "Sets opinion parameters and checks their validity"
         # b,d,u should never be touched directly to avoid having invalid opinions
         self._b = belief
         self._d = disbelief
         self._u = uncertainty
-        self._a = base_rate
+
+        if base_rate != -1:
+            self._a = base_rate
+
         self.validate_opinion()
 
     def validate_opinion(self):
@@ -41,6 +44,25 @@ class Opinion:
             self = copy.copy(discounted)
 
         return discounted
+
+    def modify_trust(self, offset: float, inplace=False):
+        "Returns the same opinion with b' = b + offset and d' = d - offset"
+        # The max positive offset is the disbelief (because d can't be negative)
+        if offset >= 0:
+            offset = min(offset, self._d)
+        # Same logic, we get max negative offset = belief
+        elif offset < 0:
+            offset = max(offset, self._b)
+
+        offset_opinion = copy.copy(self)
+        offset_opinion._b += offset
+        offset_opinion._d -= offset
+
+        if inplace:
+            self._b = offset_opinion._b
+            self._d = offset_opinion._d
+
+        return offset_opinion
 
     def projected_probability(self) -> float:
         return self._b + self._a * self._u
@@ -103,15 +125,77 @@ def average_fusion(all_opinions: list | tuple) -> Opinion:
     return fused
 
 
-class EBSL:
-    "EBSL: Ensemble Binomial Subjective Logic"
+class BSL_SM:
+    "BSLC: Ensemble Binomial Subjective Logic - Single Model"
 
     def __init__(self, model, trust_opinion: Opinion) -> None:
         self.model = model
         self.trust_opinion = trust_opinion
         self.trust_offset = 0
+        self.opinion = Opinion()
+        self.conflict = 0
 
     def get_opinion(self, data):
-        _, p = self.model.predict_proba(data)
+        "Generates the belief opinion of this model based on the prediction probability and model trust"
+        probabilities = self.model.predict_proba(data)
+        p = probabilities[1]
         scale_factor = 1+self.trust_opinion._u
-        return Opinion(p/scale_factor, (1-p)/scale_factor, self.trust_opinion._u/scale_factor)
+        self.opinion.set_parameters(
+            p/scale_factor, (1-p)/scale_factor, self.trust_opinion._u/scale_factor)
+        return self.opinion
+
+    def get_discounted_opinion(self):
+        "Calculates the discounted opinion according to the trust opinion modified with trust offset"
+        modified_trust = self.trust_opinion.modify_trust(self.trust_offset)
+        return self.opinion.trust_discounting(modified_trust)
+
+    def set_prior_probability(self, probability: float):
+        self.trust_opinion.set_base_rate(probability)
+
+
+class EBSL:
+    "EBSL: Ensemble Binomial Subjective Logic"
+
+    def __init__(self) -> None:
+        self.slmodels = []
+        self.reference_opinion = Opinion()
+
+    def add_model(self, model: BSL_SM):
+        self.slmodels.append(model)
+
+    def get_all_opinions_and_ref(self, data):
+        opinions = []
+        for slmodel in self.slmodels:
+            opinions.append(slmodel.get_opinion(data))
+        self.reference_opinion = average_fusion(opinions)
+
+    def calculate_conflict(self):
+        for slmodel in self.slmodels:
+            slmodel.conflict = slmodel.opinion.calculate_conflict(
+                self.reference_opinion)
+
+    def update_models_trust(self):
+        pass  # for now
+
+    def get_final_prediction(self):
+        "Calculates the final prediction using discounted opinions"
+        discounted_opinions = []
+        for slmodel in self.slmodels:
+            discounted_opinions.append(slmodel.get_discounted_opinion())
+
+        final_opinion = average_fusion(discounted_opinions)
+        # The base rate should be the same everywhere
+        final_opinion.set_base_rate(discounted_opinions[0]._a)
+        prob = final_opinion.projected_probability()
+        # Set the base rate for all models to be this projected probability
+        for slmodel in self.slmodels:
+            slmodel.opinion.set_base_rate(prob)
+
+        return round(prob)
+
+    def run_once(self, data) -> int:
+        # First run inference for each model, estimate opinions and their average
+        self.get_all_opinions_and_ref(data)
+        self.calculate_conflict()
+        self.update_models_trust()
+        return self.get_final_prediction()
