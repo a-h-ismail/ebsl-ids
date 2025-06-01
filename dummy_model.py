36a5d1205d527b098f25c390ea9decbe58fab26b
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2025-06-01 09:40:45 +0300

    Fix behavior to act more like sklearn when handling many samples

--- dummy_model.py
+++ dummy_model.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import numpy as np
+
 
 class DModel:
     "A very simple binary classifier model emulator. Predictions are generated according to a predefined sequence"
@@ -18,18 +20,43 @@ class DModel:
             if prob < 0 or prob > 1:
                 raise ValueError("All probabilitites should be in range [0;1]")
 
-    def predict(self, x):
+    def _increment_index(self):
+        "Increments the index, automatically rolling over at the sequence end"
+        if self.index == self.max_index:
+            self.index = 0
+        else:
+            self.index += 1
+
+    def _predict_one(self):
         # x is ignored intentionally
         prob = self.probability_seq[self.index]
-        self.index += 1
+        self._increment_index()
         return round(prob)
 
-    def predict_proba(self, x):
+    def _predict_proba_one(self):
         # Provide the raw probability of both classes, intended to imitate sklearn
         probs = (1-self.probability_seq[self.index],
                  self.probability_seq[self.index])
-        self.index += 1
+        self._increment_index()
         return probs
+
+    def predict(self, x) -> np.ndarray:
+        nb_rows = len(x)
+        results = np.empty(nb_rows)
+        for i in range(nb_rows):
+            results[i] = self._predict_one()
+        return results
+
+    def predict_proba(self, x) -> np.ndarray:
+        nb_rows = len(x)
+        results = np.empty((nb_rows, 2))
+
+        for i in range(nb_rows):
+            out = self._predict_proba_one()
+            results[i][0] = out[0]
+            results[i][1] = out[1]
+
+        return results
 
     def reset(self):
         self.index = 0
