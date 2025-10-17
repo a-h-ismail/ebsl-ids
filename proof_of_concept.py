cb5b7bd9a9cd52e2f0d19ce8436201b5479aaa83
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2025-10-17 16:00:26 +0300

    Add uncertainty and penalty-bonus plots

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -18,18 +18,18 @@ m1 = dm.DModel((0.1, 0.1, 0.15, 0.77, 0.8, 0.69, 0.76, 0.81))
 m2 = dm.DModel((0.05, 0.1, 0.2, 0.1, 0.3, 0.33, 0.81, 0.29))
 m3 = dm.DModel((0.2, 0.1, 0.1, 0.22, 0.26, 0.4, 0.37, 0.5))
 
-# Initial trust values
-o1 = sl.Opinion(0.95, 0.02, 0.03)
-o2 = sl.Opinion(0.85, 0.1, 0.05)
-o3 = sl.Opinion(0.8, 0.15, 0.05)
 
 # Create a new ensemble classifier and set the models
 eclassifier = sl.EBSL(_debug=True, base_rate_choice="prior", trust_restore_speed=0.5,
                       conflict_threshold=0.2, max_penalty=0.7)
 
-eclassifier.add_model(sl.BSL_SM(m1, None, o1, "1"))
-eclassifier.add_model(sl.BSL_SM(m2, None, o2, "2"))
-eclassifier.add_model(sl.BSL_SM(m3, None, o3, "3"))
+eclassifier.add_model(sl.BSL_SM(m1, None, None, "1"))
+eclassifier.add_model(sl.BSL_SM(m2, None, None, "2"))
+eclassifier.add_model(sl.BSL_SM(m3, None, None, "3"))
+
+eclassifier.get_model_by_name("1").trust_from_mcc(0.93)
+eclassifier.get_model_by_name("2").trust_from_mcc(0.91)
+eclassifier.get_model_by_name("3").trust_from_mcc(0.87)
 
 # Comment/uncomment the next line to see the bonus effect on the first model
 # eclassifier.get_model_by_name("1").set_bonuses(0, 0.7)
@@ -47,15 +47,17 @@ nb_samples = len(m1.probability_seq)
 x = np.linspace(0, nb_samples-1, nb_samples)
 
 # Create figure and axis objects with 2 rows and 2 columns
-fig, axs = plt.subplots(2, 2, figsize=(15, 15))
+fig, axs = plt.subplots(2, 3, figsize=(15, 15))
 fig.canvas.manager.set_window_title('EBSL Debug Statistics')  # type: ignore
 
 axs[0, 0].set_title("Individual Model Class 1 Probabilities")
 axs[0, 1].set_title("Distance to Average Conflict")
-axs[1, 0].set_title("Weights")
-axs[1, 1].set_title("Predicted Label (or Class 1 Probability)")
+axs[0, 2].set_title("Weights")
+axs[1, 0].set_title("Uncertainty in Information Opinion")
+axs[1, 1].set_title("Penalty minus Bonus")
+axs[1, 2].set_title("Final prediction probability")
 axs[0, 0].set_ylim([0, 1])
-axs[1, 1].set_ylim([0, 1])
+axs[1, 2].set_ylim([0, 1])
 
 # Plot debug metrics
 for i in range(len(eclassifier.slmodels)):
@@ -64,24 +66,30 @@ for i in range(len(eclassifier.slmodels)):
     axs[0, 0].plot(x, eclassifier.slmodels[i].prediction_cache, label=name, marker="o")
     # Dist to avg
     axs[0, 1].plot(x, eclassifier._slm_dist_to_avg[i], label=name, marker="o")
-    # Then weights
-    axs[1, 0].plot(x, eclassifier._slm_weights[i], label=name, marker="o")
+    # Weights
+    axs[0, 2].plot(x, eclassifier._slm_weights[i], label=name, marker="o")
+    # Uncertainty
+    axs[1, 0].plot(x, eclassifier._slm_uncertainty[i], label=name, marker="o")
+    # And modifiers
+    axs[1, 1].plot(x, eclassifier._slm_penalties[i], label=name, marker="o")
 
 # The conflict threshold line
 axs[0, 1].axline((0, eclassifier.conflict_threshold), slope=0, color='r', label="Conflict Threshold")
 # The last prediction weight
 if eclassifier.base_rate_choice_str == "prior":
-    axs[1, 0].plot(x, eclassifier._slm_weights[len(eclassifier.slmodels)], label="Last prediction", marker="x")
+    axs[0, 2].plot(x, eclassifier._slm_weights[len(eclassifier.slmodels)], label="Last prediction", marker="x")
+# And uncertainty
+axs[1, 0].plot(x, eclassifier._slm_uncertainty[len(eclassifier.slmodels)], label="Final opinion", marker="o")
 
 # Prediction comparison between voting classifiers
-axs[1, 1].plot(x, predicted, label="EBSL", marker='x')
-axs[1, 1].plot(x, eclassifier._hard_vote(), label="Hard Vote", marker='x')
-axs[1, 1].plot(x, eclassifier._soft_vote_prob(), label="Soft Vote", marker='x')
-axs[1, 1].axline((0, 0.5), slope=0, color='r', label="Probability Threshold")
+axs[1, 2].plot(x, predicted, label="EBSL", marker='x')
+axs[1, 2].plot(x, eclassifier._hard_vote(), label="Hard Vote", marker='x')
+axs[1, 2].plot(x, eclassifier._soft_vote_prob(), label="Soft Vote", marker='x')
+axs[1, 2].axline((0, 0.5), slope=0, color='r', label="Probability Threshold")
 
 # Write the legends
 for i in range(2):
-    for j in range(2):
+    for j in range(3):
         box = axs[i, j].get_position()
         axs[i, j].set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
         axs[i, j].legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=3)
